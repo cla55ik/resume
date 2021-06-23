92cf8e35f0ca9525c80c2e49e5cdc6cf61c2ba39
Ivan <ivan5420@yandex.ru>
2021-06-23 12:30:28 +0300

    add Head component clear

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <Head />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Head from './components/Head.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Head
   }
 }
 </script>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body{
+  margin: 0px 15px;
 }
 </style>
---
+++ src/components/Head.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="typewriter">
+    IVAN/CHELNOKOV/WEB/DEVELOPER
+</div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+    div.typewriter{
+        color: #fff;
+        text-align: left;
+        
+    }
+</style>
